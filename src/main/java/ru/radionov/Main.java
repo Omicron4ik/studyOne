d672e0e0a2ea83dd61c89e4c9a43279085b19ef1
aleksey.radionov <Aleksey.Radionov@newdv.ru>
2019-11-27 13:53:10 +0300

    add check

--- src/main/java/ru/radionov/Main.java
+++ src/main/java/ru/radionov/Main.java
@@ -1,21 +1,31 @@
 package ru.radionov;
 
+import java.util.regex.Pattern;
 import java.util.Scanner;
 
 public class Main {
+    public static String readOneStringFromUser (int j) {
+        System.out.println("enter pls " + j  + " NUMBER");
+        Scanner inputNum = new Scanner(System.in);
+        return inputNum.next();
+    }
 
-    public static void main(String[] args) {
+    public static int []  readMultiStringFromUser() {
         int [] arr =  new int [4];
-
         for (int i = 0; i <= 3; i++) {
             int j = i + 1;
-            System.out.println("enter pls " + j  + " numeral");
-            Scanner inputNum = new Scanner(System.in);
-            int num;
-            num = inputNum.nextInt();
-            arr[i] = num;
-            inputNum.close();
+            String inputStr = readOneStringFromUser(j);
+            if (Pattern.matches("\\d+",inputStr)) {
+                int num = Integer.parseInt(inputStr);
+                arr[i] = num;
+            } else {
+                System.out.println("OLOLO it's not a NUMBER, set '0'");
+            }
         }
+        return arr;
+    }
+
+    public static void showResults(int [] arr) {
         for (int i = 0; i <= 3; i++) {
             int j = i + 1;
             System.out.println("Your " + j + " numeral is: " + arr[i]);
@@ -25,7 +35,15 @@ public class Main {
                 System.out.println("and it is LESS than 10, it is '" + arr[i] + "'");
             } else System.out.println("and it is exactly 10");
         }
-        System.out.println("Well done!");
+    }
+    public static void main(String[] args) {
+        int [] arr = readMultiStringFromUser();
+        if (arr[3] != 0) {
+            showResults(arr);
+            System.out.println("Well done!");
+        } else {
+            System.out.println("Sorry, no result");
+        }
     }
 }
 
